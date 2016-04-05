8d80a8cc8439c298e205afff0671f77bd50fe4c0
olivia-leach <olivia.i.m.leach@gmail.com>
2016-04-05 14:32:46 -0400

    Create new file for game api events
    Add generic functions for success and failure

---
+++ assets/scripts/game-ui.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// const app = require('./app-data.js');
+
+const success = (data) => {
+  console.log(data);
+};
+
+const failure = (error) => {
+  console.error(error);
+};
+
+module.exports = {
+  failure,
+  success,
+};
