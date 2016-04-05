660054abe4f96ff881b1ff785d53ccf6a630b459
olivia-leach <olivia.i.m.leach@gmail.com>
2016-04-04 23:14:20 -0400

    Create functions to be used in AJAX queries
    * signOutSuccess -> logs to the console that sign out was successful, hides/shows
    appropriate log in buttons, hides welcome statement.
    * signInSuccess -> logs to the console that sign in was successful, stores user
    data in app.user, hides/shows appropriate log in buttons, shows welcome statement
    and adds user's email to the statement.

---
+++ assets/scripts/ui.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const app = require('./app-data.js');
+
+const signOutSuccess = (data) => {
+  app.user = data;
+  console.log("logged out");
+  console.log(app);
+  $("#signInButton").show();
+  $("#registerButton").show();
+  $("#signOutButton").hide();
+  $("#welcome").hide();
+};
+
+const signInSuccess = (data) => {
+  app.user = data.user;
+  console.log(data.user);
+  console.log(data.user.email + " logged in");
+  $("#signInButton").hide();
+  $("#registerButton").hide();
+  $("#signOutButton").show();
+  $("#welcome").show().text("Welcome, " + app.user.email + "!");
+};
+
+const success = (data) => {
+  console.log(data);
+};
+
+const failure = (error) => {
+  console.error(error);
+  if(error.status === 401) {
+    $("h1").text("Incorrect password.");
+  }
+  // $("h1").text("Account already exists");
+};
+
+module.exports = {
+  failure,
+  success,
+  signInSuccess,
+  signOutSuccess
+};
