5a8895beb56408bd7ee164cc2f7ed18a07eb2e65
olivia-leach <olivia.i.m.leach@gmail.com>
2016-04-04 23:05:40 -0400

    Add on document ready statement
    Link to event handlers

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,12 @@
 'use strict';
 
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
+const authEvents = require('./events.js');
+const app = require('./app-data.js');
 
-// use require without a reference to ensure a file is bundled
-require('./example');
+
+// On document ready
+$(() => {
+  authEvents.addHandlers();
+  $("#signOutButton").hide();
+  $("#welcome").hide();
+});
