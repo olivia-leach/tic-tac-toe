f227947b3a8b22d0afdb5ebaa0a41bc0165123d5
olivia-leach <olivia.i.m.leach@gmail.com>
2016-04-06 16:55:13 -0400

    Make get history function iterate through history
    Calculate and display stats
    Add info to table

--- assets/scripts/game-ui.js
+++ assets/scripts/game-ui.js
@@ -18,8 +18,70 @@ const updateGameSuccess = (data) => {
   console.log(app.game.cells);
 };
 
+const indexGameSuccess = (data) => {
+  console.log(data);
+  console.log("game history saved");
+  app.gameHistory = data;
+  console.log(app.gameHistory);
+  $("#gamesPlayed").text(app.gameHistory.games.length);
+  $("#game-history-table").show();
+  let winCount = 0;
+  for (let i = 0; i < app.gameHistory.games.length; i++) {
+
+    let playedAs = "o";
+    if (app.gameHistory.games[i].player_x.id === app.user.id) {
+      playedAs = "x";
+    }
+
+    let over = "Yes";
+    if (app.gameHistory.games[i].over === false) {
+      over = "No";
+    }
+
+    let board = app.gameHistory.games[i].cells;
+
+    if (over === "No") {
+      app.gameHistory.games[i].winner = "---";
+    } else if (board[0] === board[1] && board[0] === board[2] && board[0] !== '') {
+      app.gameHistory.games[i].winner = board[0];
+    } else if (board[3] === board[4] && board[3] === board[5] && board[3] !== '') {
+      app.gameHistory.games[i].winner = board[3];
+    } else if (board[6] === board[7] && board[6] === board[8] && board[6] !== '') {
+      app.gameHistory.games[i].winner = board[6];
+    } else if (board[0] === board[3] && board[0] === board[6] && board[0] !== '') {
+      app.gameHistory.games[i].winner = board[0];
+    } else if (board[1] === board[4] && board[1] === board[7] && board[1] !== '') {
+      app.gameHistory.games[i].winner = board[1];
+    } else if (board[2] === board[5] && board[2] === board[8] && board[2] !== '') {
+      app.gameHistory.games[i].winner = board[2];
+    } else if (board[0] === board[4] && board[0] === board[8] && board[0] !== '') {
+      app.gameHistory.games[i].winner = board[0];
+    } else if (board[2] === board[4] && board[2] === board[6] && board[2] !== '') {
+      app.gameHistory.games[i].winner = board[2];
+    } else {
+      app.gameHistory.games[i].winner = "Cat";
+    }
+
+    $("#game-history-table").find('tbody')
+      .append($('<tr>')
+        .append($('<td>').text(i + 1))
+        .append($('<td>').text(playedAs))
+        .append($('<td>').text(over))
+        .append($('<td>').text(app.gameHistory.games[i].winner))
+      );
+
+      if (app.gameHistory.games[i].winner === playedAs) {
+        winCount += 1;
+      }
+
+  }
+
+  $("#gamesWon").text(winCount);
+  $("#percent").text(Math.round(100 * (winCount/app.gameHistory.games.length),0) + '%');
+
+};
+
 const success = (data) => {
-  console.log("game updated");
   console.log(data);
 };
 
@@ -31,5 +93,6 @@ module.exports = {
   failure,
   success,
   newGameSuccess,
-  updateGameSuccess
+  updateGameSuccess,
+  indexGameSuccess
 };
